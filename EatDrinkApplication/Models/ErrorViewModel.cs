6a29cc49282168f2d7a83914493a21a607eb579b
Justin Haldeman <jmanh14@gmail.com>
2020-04-14 07:28:44 -0500

    Init

---
+++ EatDrinkApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EatDrinkApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
